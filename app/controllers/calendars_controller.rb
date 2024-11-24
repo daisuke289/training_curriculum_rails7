a54f27b03eac8d2132c27d7c1189cf1c3aadae3b
daisuke289 <daisuke289@gmail.com>
2024-11-24 22:11:50 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -25,23 +25,29 @@ class CalendarsController < ApplicationController
   end
 
   def getWeek
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
-    @todays_date = Date.today
-    @week_days = []
-
-    plans = Plan.where(date: @todays_date..@todays_date + 6)
-
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)'] # 曜日配列
+  
+    @todays_date = Date.today # 今日の日付を取得
+    @week_days = [] # 1週間の日付情報を格納する配列
+  
+    plans = Plan.where(date: @todays_date..@todays_date + 6) # 今日から1週間分の予定を取得
+  
     7.times do |x|
+      # 今日からx日後の日付に対応する予定を取得
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+  
+      # 曜日を計算し、days ハッシュに追加
       days = {
-        month: (@todays_date + x).month,
-        date: (@todays_date + x).day,
-        plans: today_plans
+        month: (@todays_date + x).month,          # 月
+        date: (@todays_date + x).day,            # 日
+        plans: today_plans,                      # 予定
+        wday: wdays[(@todays_date + x).wday]     # 曜日
       }
+  
+      # days を @week_days 配列に追加
       @week_days.push(days)
     end
   end
